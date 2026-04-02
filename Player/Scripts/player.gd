eb797d4e50cd0a6dd9528a89a8be84f3a87d7ee6
Sanchirbayar <bagaezwin@gmail.com>
2026-04-02 08:14:32 +0800

    Added idle and walking animations

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,22 +1,25 @@
 class_name Player extends CharacterBody2D
 
-var move_speed : float = 200.0
+var move_speed : float = 100.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-
+func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
-	velocity  = direction*move_speed 
+	velocity = direction.normalized() * move_speed
 
-	pass
+	if direction != Vector2.ZERO:
+		animated_sprite.play("walk")
+		if direction.x != 0.0:
+			animated_sprite.flip_h = direction.x < 0.0
+	else:
+		animated_sprite.play("idle")
 
-func _physics_process(delta: float) -> void:
 	move_and_slide()
