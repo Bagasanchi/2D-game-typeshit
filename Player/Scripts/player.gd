f4d5e6f0e04d95c7917839759303ca7b6f860061
Sanchirbayar <bagaezwin@gmail.com>
2026-04-06 11:53:07 +0800

    Fixed the player turning system

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,11 +1,20 @@
 class_name Player extends CharacterBody2D
 
 var move_speed : float = 100.0
+@export var flip_visual_correction_px: float = 7.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var _sprite_base_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_sprite_base_position = animated_sprite.position
+
+
+func _set_facing(is_left: bool) -> void:
+	animated_sprite.flip_h = is_left
+	# Compensate for asymmetric sprite padding so turning does not "jump" horizontally.
+	var correction := flip_visual_correction_px if is_left else -flip_visual_correction_px
+	animated_sprite.position = _sprite_base_position + Vector2(correction, 0.0)
 
 
 func _physics_process(_delta: float) -> void:
@@ -18,7 +27,7 @@ func _physics_process(_delta: float) -> void:
 	if direction != Vector2.ZERO:
 		animated_sprite.play("walk")
 		if direction.x != 0.0:
-			animated_sprite.flip_h = direction.x < 0.0
+			_set_facing(direction.x < 0.0)
 	else:
 		animated_sprite.play("idle")
 
