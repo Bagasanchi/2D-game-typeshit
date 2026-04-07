30e48893aa877f88d3a5e96a173b23e8abf06bb6
Sanchirbayar <bagaezwin@gmail.com>
2026-04-07 23:21:44 +0800

    Deleted the position fixing stuff that used to work for the old character

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,7 +1,6 @@
 class_name Player extends CharacterBody2D
 
 var move_speed : float = 100.0
-@export var flip_visual_correction_px: float = 7.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var _sprite_base_position: Vector2
 
@@ -9,12 +8,8 @@ var _sprite_base_position: Vector2
 func _ready() -> void:
 	_sprite_base_position = animated_sprite.position
 
-
 func _set_facing(is_left: bool) -> void:
 	animated_sprite.flip_h = is_left
-	# Compensate for asymmetric sprite padding so turning does not "jump" horizontally.
-	var correction := flip_visual_correction_px if is_left else -flip_visual_correction_px
-	animated_sprite.position = _sprite_base_position + Vector2(correction, 0.0)
 
 func _physics_process(_delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
@@ -23,7 +18,9 @@ func _physics_process(_delta: float) -> void:
 
 	velocity = direction.normalized() * move_speed
 
-	if direction != Vector2.ZERO:
+	if Input.is_physical_key_pressed(KEY_SPACE):
+		animated_sprite.play("jump")
+	elif direction != Vector2.ZERO:
 		animated_sprite.play("run")
 		if direction.x != 0.0:
 			_set_facing(direction.x < 0.0)
