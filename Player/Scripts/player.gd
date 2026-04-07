d7c9932785a760ed3cd1b02e3f9f82601d4b2d2f
Sanchirbayar <bagaezwin@gmail.com>
2026-04-07 23:23:03 +0800

    removed the up and down keybind

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -14,7 +14,6 @@ func _set_facing(is_left: bool) -> void:
 func _physics_process(_delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
 	velocity = direction.normalized() * move_speed
 
